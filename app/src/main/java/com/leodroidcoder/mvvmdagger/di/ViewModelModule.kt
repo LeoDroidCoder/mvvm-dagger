9a4d5548b85bf42f2530f5e185f8004a9e0ec799
lustenko <leonid.ustenko@exness.com>
2018-12-10 19:37:27 +0200

    Moved ViewModel bindings to ViewModelModule

--- app/src/main/java/com/leodroidcoder/mvvmdagger/di/ViewModelModule.kt
+++ app/src/main/java/com/leodroidcoder/mvvmdagger/di/ViewModelModule.kt
@@ -1,14 +1,29 @@
 package com.leodroidcoder.mvvmdagger.di
 
+import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import com.leodroidcoder.mvvmdagger.fragment1.ViewModel1
+import com.leodroidcoder.mvvmdagger.fragment2.ViewModel2
 import com.leodroidcoder.mvvmdagger.mvvm.ViewModelFactory
+import com.leodroidcoder.mvvmdagger.mvvm.ViewModelKey
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
 
     @Binds
-    abstract fun bind(factory: ViewModelFactory): ViewModelProvider.Factory
+    abstract fun bindFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ViewModel1::class)
+    abstract fun bindVm1(viewModel: ViewModel1): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ViewModel2::class)
+    abstract fun bindVm2(viewModel: ViewModel2): ViewModel
 
 }
--- app/src/main/java/com/leodroidcoder/mvvmdagger/fragment2/Fragment2Provider.kt
+++ app/src/main/java/com/leodroidcoder/mvvmdagger/fragment2/Fragment2Provider.kt
@@ -1,11 +1,7 @@
 package com.leodroidcoder.mvvmdagger.fragment2
 
-import android.arch.lifecycle.ViewModel
-import com.leodroidcoder.mvvmdagger.mvvm.ViewModelKey
-import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import dagger.multibindings.IntoMap
 
 @Module
 abstract class Fragment2Provider {
@@ -13,8 +9,4 @@ abstract class Fragment2Provider {
     @ContributesAndroidInjector(modules = [Fragment2Module::class])
     abstract fun provideFragment(): Fragment2
 
-    @Binds
-    @IntoMap
-    @ViewModelKey(ViewModel2::class)
-    abstract fun bind(viewModel: ViewModel2): ViewModel
 }
